2951b89568db8d545f0ac8490e6d2a27a1e583ce
petehunt <floydophone@gmail.com>
2013-08-09 02:04:54 -0700

    comment the code

--- react-transitions.js
+++ react-transitions.js
@@ -6,6 +6,7 @@ var END_EVENT_NAMES = [
   'transitionend', 'webkitTransitionEnd', 'otransitionend', 'MSTransitionEnd'
 ];
 
+// Basic functions to manipulate raw DOM CSS classes
 // It's cool to use classList because pre-IE10 doesn't have CSS3 stuff
 function removeClasses(node, classes) {
   if (node.classList) {
@@ -19,26 +20,38 @@ function addClass(node, className) {
   }
 }
 
+// Perform an actual DOM animation. This takes care of a few things:
+// - Adding the second CSS class to trigger the transition
+// - Listening for the finish event
+// - Cleaning up the css (unless noReset is true)
 function animateDOMNode(node, animationName, animationType, noReset, finishCallback) {
   var className = animationName + '-' + animationType;
   var activeClassName = className + '-active';
-    // remove when done
+
   function endListener() {
     if (!noReset) {
+      // Usually this means you're about to remove the node if you want to leave it
+      // in its animated state.
       removeClasses(node, [className, activeClassName]);
     }
     END_EVENT_NAMES.map(function(eventName) {
       node.removeEventListener(eventName, endListener, false);
     });
+    // Usually this optional callback is used for informing an owner of an exit animation
+    // and telling it to remove the child.
     finishCallback && finishCallback();
   }
   END_EVENT_NAMES.map(function(eventName) {
     node.addEventListener(eventName, endListener, false);
   });
   addClass(node, className);
+  // Need to do this to actually trigger a transition.
   setTimeout(addClass.bind(this, node, activeClassName), 0);
 }
 
+// This component is simply responsible for watching when its single child
+// changes to undefined and animating the old child out. It does this by recording
+// its old child in savedChildren when it detects this event is about to occur.
 var AnimateSingle = React.createClass({
   componentWillReceiveProps: function(nextProps) {
     if (!nextProps.children && this.props.children) {
@@ -58,6 +71,9 @@ var AnimateSingle = React.createClass({
   }
 });
 
+// To save memory props.children can either be an object holding multiple children or
+// a React component or a primitive value. This ensures that we have an object with
+// keys.
 function getChildren(props) {
   var pChildren = props.children;
   if (!pChildren || typeof pChildren !== 'object' || pChildren.isMounted) {
@@ -66,6 +82,8 @@ function getChildren(props) {
   return pChildren;
 }
 
+// Some sugar to get the key set. Basically the same as the children object except
+// it does not have references to big objects in it.
 function getKeySet(children) {
   var keySet = {};
   for (var key in children) {
@@ -77,6 +95,12 @@ function getKeySet(children) {
   return keySet;
 }
 
+// OK, here is where things start to get intense. When you're adding or removing children
+// some may be added or removed in the same pass. We want to show *both* since we want to
+// simultaneously animate in and out. This is our best-guess at how it should look with
+// respect to ordering. In the future we may expose some of the utilities in ReactMultiChild
+// to make this easy, but for now React itself does not directly have this concept so we
+// implement it here:
 function mergeKeySets(prev, next) {
   var keySet = {};
   var prevKeys = Object.keys(prev).concat(['.mergeKeySets-tail']);
@@ -107,12 +131,17 @@ function mergeKeySets(prev, next) {
   return keySet;
 }
 
+// Here is the end-user API!
 var Animate = React.createClass({
   getInitialState: function() {
+    // Remember: we want to show all components animating in AND out at
+    // the same time. This state variable holds the union of both the old
+    // child keys and the new ones.
     return {currentKeys: getKeySet(getChildren(this.props))};
   },
   componentWillReceiveProps: function(nextProps) {
     if (this.props.children !== nextProps.children) {
+      // This is where we actually take the union of both sets of keys
       this.setState({
         currentKeys: mergeKeySets(
           this.state.currentKeys,
@@ -125,6 +154,11 @@ var Animate = React.createClass({
     var children = {};
     var pChildren = getChildren(this.props);
     for (var key in this.state.currentKeys) {
+      // Here is how we keep the nodes in the DOM. AnimateSingle knows how
+      // to hold onto its child if it changes to undefined. Here, we may look
+      // up an old key in the new children, and it may switch to undefined. React's
+      // reconciler will keep the AnimateSingle instance alive such that we can
+      // animate it.
       children[key] = AnimateSingle({
         name: this.props.name,
         onDoneLeaving: this.handleDoneLeaving.bind(this, key)
@@ -133,14 +167,21 @@ var Animate = React.createClass({
     return children;
   },
   handleDoneLeaving: function(key) {
+    // When the leave animation finishes, we should blow away the actual DOM node.
     var currentKeys = this.state.currentKeys;
-    // TODO: this is a mutation, but I know what I'm doing.
+    // Normally you don't mutate state, but the alternative is to create a new object
+    // for no reason. This is easier and faster.
     delete currentKeys[key];
     this.setState({currentKeys: currentKeys});
   },
+  getDefaultProps: function() {
+    return {component: React.DOM.div};
+  },
   render: function() {
-    var componentClass = this.props.component || React.DOM.div;
-    return this.transferPropsTo(componentClass(null, this.renderChildren()));
+    // The one shortcoming of the API is that this component needs to render a real DOM node
+    // into the document. Sometimes you want a <div> to contain them, other times you want
+    // a <ul>. You can pass a component prop to control this (we default to <div>).
+    return this.transferPropsTo(this.props.component(null, this.renderChildren()));
   }
 });
 
